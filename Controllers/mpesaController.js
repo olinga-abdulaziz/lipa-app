d380911644f526b259150fa1e03af1a48c8b1423
olinga-abdulaziz <60089431+olinga-abdulaziz@users.noreply.github.com>
2022-01-24 14:18:36 +0300

    Update mpesaController.js

--- Controllers/mpesaController.js
+++ Controllers/mpesaController.js
@@ -56,7 +56,7 @@ exports.stkpush=(req,res) =>{
         "PartyA":"254769210601",    
         "PartyB":"174379",    
         "PhoneNumber":"254769210601",    
-        "CallBackURL":"http://lipaapp.herokuapp.com/callback",    
+        "CallBackURL":"https://lipaapp.herokuapp.com/callback",    
         "AccountReference":"Abdula acc",    
         "TransactionDesc":"Lipanampesa"
     }
